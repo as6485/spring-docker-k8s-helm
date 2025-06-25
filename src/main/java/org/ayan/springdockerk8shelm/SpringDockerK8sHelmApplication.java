968a12d1f21ecea3b10b304c971312154dd425a5
Ayan Sengupta <as6485@gmail.com>
2025-06-25 18:41:19 +0530

    Initial commit

---
+++ src/main/java/org/ayan/springdockerk8shelm/SpringDockerK8sHelmApplication.java
@@ -0,0 +1,13 @@
+package org.ayan.springdockerk8shelm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerK8sHelmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDockerK8sHelmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ayan/springdockerk8shelm/SpringDockerK8sHelmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ayan.springdockerk8shelm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerK8sHelmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
